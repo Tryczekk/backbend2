91d0eb99d4810c93ee99c51c8e41dc0c1b7005c7
Tryczekk <paprykca@gmail.com>
2025-06-15 12:17:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 const TOKENS_FILE = path.join(__dirname, 'tokens.json');
